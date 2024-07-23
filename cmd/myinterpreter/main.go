02ea2ca250d51116e215550308130ea8f87d0832
hrantm <hrant.mirijanyan@gmail.com>
2024-07-23 15:24:49 -0700

    refactor

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -9,7 +9,8 @@ import (
 type TokenType int
 
 const (
-	LEFT_PAREN TokenType = iota
+	UNKNOWN TokenType = iota
+	LEFT_PAREN
 	RIGHT_PAREN
 	LEFT_BRACE
 	RIGHT_BRACE
@@ -19,6 +20,8 @@ const (
 	PLUS
 	SEMICOLON
 	STAR
+	EQUAL
+	EQUAL_EQUAL
 	EOF
 )
 
@@ -28,7 +31,7 @@ type Token struct {
 }
 
 func (t TokenType) String() string {
-	tokenNames := []string{"LEFT_PAREN", "RIGHT_PAREN", "LEFT_BRACE", "RIGHT_BRACE", "COMMA", "DOT", "MINUS", "PLUS", "SEMICOLON", "STAR", "EOF"}
+	tokenNames := []string{"UNKNOWN", "LEFT_PAREN", "RIGHT_PAREN", "LEFT_BRACE", "RIGHT_BRACE", "COMMA", "DOT", "MINUS", "PLUS", "SEMICOLON", "STAR", "EQUAL", "EQUAL_EQUAL", "EOF"}
 	if t < LEFT_PAREN || t > EOF {
 		return "Unknown"
 	}
@@ -75,85 +78,94 @@ func main() {
 	const maxCapacity = 1024 * 1024 // 1 MB
 	buf := make([]byte, maxCapacity)
 	scanner.Buffer(buf, maxCapacity)
+	// error := false
+	// // if len(fileContents) > 0 {
+	line_num := 1
 	error := false
-	// if len(fileContents) > 0 {
 	for scanner.Scan() {
 		line := scanner.Text()
-
+		var t Token
 		for _, val := range line {
-			// line_num := i + 1
-			line_num := 1
-			// fmt.Println(line_num, string(val))
-			char := string(val)
+			t = getToken(val)
 
-			var t Token
-			switch char {
-			case "(":
-				t = Token{
-					Type:   TokenType(0),
-					Lexeme: "(",
-				}
-
-			case ")":
-				t = Token{
-					Type:   TokenType(1),
-					Lexeme: ")",
-				}
-			case "{":
-				t = Token{
-					Type:   TokenType(2),
-					Lexeme: "{",
-				}
-			case "}":
-				t = Token{
-					Type:   TokenType(3),
-					Lexeme: "}",
-				}
-			case ",":
-				t = Token{
-					Type:   TokenType(4),
-					Lexeme: ",",
-				}
-			case ".":
-				t = Token{
-					Type:   TokenType(5),
-					Lexeme: ".",
-				}
-			case "-":
-				t = Token{
-					Type:   TokenType(6),
-					Lexeme: "-",
-				}
-			case "+":
-				t = Token{
-					Type:   TokenType(7),
-					Lexeme: "+",
-				}
-			case ";":
-				t = Token{
-					Type:   TokenType(8),
-					Lexeme: ";",
-				}
-			case "*":
-				t = Token{
-					Type:   TokenType(9),
-					Lexeme: "*",
-				}
-			default:
-				error = true
-				message := fmt.Sprintf("[line %v] Error: Unexpected character: %v", line_num, char)
+			if t.Type != 0 {
+				fmt.Fprintln(os.Stdout, t.Type.String(), t.Lexeme, "null")
+			} else {
+				message := fmt.Sprintf("[line %v] Error: Unexpected character: %v", line_num, string(val))
 
 				fmt.Fprintln(os.Stderr, message)
-			}
-			if t.Lexeme != "" {
-				fmt.Fprintln(os.Stdout, t.Type.String(), t.Lexeme, "null")
-			}
+				error = true
 
+			}
 		}
+		line_num++
 	}
 	fmt.Println("EOF  null")
 	if error {
 		os.Exit(65)
 	}
+}
 
+func getToken(c rune) Token {
+	var t Token
+	switch c {
+	case '(':
+		t = Token{
+			Type:   TokenType(1),
+			Lexeme: "(",
+		}
+
+	case ')':
+		t = Token{
+			Type:   TokenType(2),
+			Lexeme: ")",
+		}
+
+	case '{':
+		t = Token{
+			Type:   TokenType(3),
+			Lexeme: "{",
+		}
+	case '}':
+		t = Token{
+			Type:   TokenType(4),
+			Lexeme: "}",
+		}
+	case ',':
+		t = Token{
+			Type:   TokenType(5),
+			Lexeme: ",",
+		}
+	case '.':
+		t = Token{
+			Type:   TokenType(6),
+			Lexeme: ".",
+		}
+	case '-':
+		t = Token{
+			Type:   TokenType(7),
+			Lexeme: "-",
+		}
+	case '+':
+		t = Token{
+			Type:   TokenType(8),
+			Lexeme: "+",
+		}
+	case ';':
+		t = Token{
+			Type:   TokenType(9),
+			Lexeme: ";",
+		}
+	case '*':
+		t = Token{
+			Type:   TokenType(10),
+			Lexeme: "*",
+		}
+	case '=':
+		t = Token{
+			Type:   TokenType(11),
+			Lexeme: "=",
+		}
+	}
+	return t
 }
